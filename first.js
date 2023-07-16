3b16797a0ebc5c75c568aa27e1067533816c7e74
AryaR2705 <139691040+AryaR2705@users.noreply.github.com>
2023-07-16 18:53:00 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,83 @@
+
+const hello = document.querySelector('.bar');
+const hello1 = document.querySelector('.middle');
+const hello2 = document.querySelector('.text');
+const hello3 = document.querySelector('.bar');
+const hello4 = document.querySelector('.right button');
+
+let isclicked=true;
+let toggleHide = function(){
+    if(isclicked){
+        hello.style.display = 'block'
+        
+        hello3.style.left = '-1px'
+        hello1.style.opacity = '40%'
+        hello2.style.opacity = '50%'
+        hello4.style.position='fixed'
+        
+
+        isclicked=false;
+    }
+
+else{
+    hello.style.display = 'block'
+    
+    hello3.style.left = '350px'
+    hello1.style.opacity = '100%'
+    hello2.style.opacity = '100%'
+    hello4.style.position='relative'
+    
+    isclicked=true;
+}
+}
+// function toggleHide(){
+//     document.getElementsByClassName('bar').classList.toggle('active');
+// }
+function enfj(){
+    location.href="#enfj"
+}
+function entj(){
+    location.href="#entj"
+}
+function enfp(){
+    location.href="#enfp"
+}
+function entp(){
+    location.href="#entp"
+}
+function infj(){
+    location.href="#infj"
+}
+function infp(){
+    location.href="#infp"
+}
+function intj(){
+    location.href="#intj"
+}
+function intp(){
+    location.href="#intp"
+}
+function istp(){
+    location.href="#istp"
+}
+function isfp(){
+    location.href="#isfp"
+}
+function istj(){
+    location.href="#istj"
+}
+function isfj(){
+    location.href="#isfj"
+}
+function esfj(){
+    location.href="#esfj"
+}
+function estj(){
+    location.href="#estj"
+}
+function estp(){
+    location.href="#estp"
+}
+function esfp(){
+    location.href="#esfp"
+}
